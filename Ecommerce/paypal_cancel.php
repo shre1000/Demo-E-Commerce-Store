ed92f0a3217f769363ee046fbc43642e2273a683
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-19 20:22:05 -0500

    Update paypal_cancel.php

--- Ecommerce/paypal_cancel.php
+++ Ecommerce/paypal_cancel.php
@@ -8,7 +8,7 @@
 
 	<h2>Payment was cancelled!</h2>
 	<h3>Your Payment was not successful, go back to our shop and try again.</h3>
-	<h3><a href="http://www.onlinetuting.com/myshop">Go to back</a></h3>
+	<h3><a href="http://www.demo.com/myshop">Go to back</a></h3>
 
 
 
